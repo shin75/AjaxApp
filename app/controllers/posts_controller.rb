fd1b2837caad80fd2d8e12dc58e3a1678e9ad9dc
shin75 <68498224+shin75@users.noreply.github.com>
2020-08-03 18:13:27 +0900

    既読機能のフロント側の記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all.order(id: "DESC")
   end
-
-
 
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
   end
-  
+
   def checked
+
+    # binding.pry
+
     post = Post.find(params[:id])
     if post.checked then
       post.update(checked: false)
@@ -21,4 +23,5 @@ class PostsController < ApplicationController
     item = Post.find(params[:id])
     render json: { post: item }
   end
+
 end
